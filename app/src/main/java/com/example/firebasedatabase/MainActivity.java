fef8d51595a255da8513020cacc59060693f0ceb
Shivam Anand <anand.shivam44@yahoo.com>
2020-03-10 11:28:23 +0530

    used Maps

--- app/src/main/java/com/example/firebasedatabase/MainActivity.java
+++ app/src/main/java/com/example/firebasedatabase/MainActivity.java
@@ -8,9 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+
+import com.example.firebasedatabase.model.User;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     EditText mAge;
     FirebaseDatabase mDatabase;
     ValueEventListener mListner;
+    ChildEventListener childListner;
     EditText mName;
     TextView mOutputText;
     DatabaseReference mRef;
@@ -35,26 +40,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         initObjects();
-        this.mRef.addValueEventListener(this.mListner);
+        //this.mRef.addValueEventListener(this.mListner);
+        mRef.addChildEventListener(childListner);
         this.Execute_Button.setOnClickListener(new OnClickListener() {
             public void onClick(View v) {
-                String obj = MainActivity.this.mName.getText().toString();
-                Log.d(MainActivity.TAG, " Update ");
-                MainActivity.this.mRef.child("user 1").removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
-                    public void onSuccess(Void aVoid) {
-                        Log.d(MainActivity.TAG, "onSuccess: TASK SUCCESSFUL");
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    public void onFailure(Exception e) {
-                        Log.d(MainActivity.TAG, "onFailure: DATA removed");
-                    }
-                });
+                String name = mName.getText().toString();
+                String age=mAge.getText().toString();
+                User user=new User(name,age);
+//                Map<String,Object> updateValue=new HashMap<>();
+//                updateValue.put("name",name);
+//                updateValue.put("age",age);
+//
+//                Log.d(MainActivity.TAG, " Update ");
+                String key=mRef.push().getKey();
+                mRef.child(key).setValue(user);
+//                mRef.child("user 1").removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+//                    public void onSuccess(Void aVoid) {
+//                        Log.d(MainActivity.TAG, "onSuccess: TASK SUCCESSFUL");
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//                    public void onFailure(Exception e) {
+//                        Log.d(MainActivity.TAG, "onFailure: DATA removed");
+//                    }
+//                });
             }
         });
     }
 
     private void initObjects() {
-        this.mListner = new ValueEventListener() {
+        mListner = new ValueEventListener() {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (Object append : dataSnapshot.getChildren()) {
                     StringBuilder sb = new StringBuilder();
@@ -72,22 +86,53 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(mainActivity, sb.toString(), Toast.LENGTH_SHORT).show();
             }
         };
+
+        childListner=new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d(TAG, "onChildAdded: CALLED");
+                User person=dataSnapshot.getValue(User.class);
+                Log.d(TAG, "Name = "+person.getName()+" Age = "+person.getAge());
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d(TAG, "onChildChanged: CALLED");
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                Log.d(TAG, "onChildRemoved: CALLED");
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d(TAG, "onChildMoved: CALLED");
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG, "onCancelled: CALLED");
+            }
+        };
     }
 
     private void init() {
-        this.Execute_Button = (Button) findViewById(R.id.execute_button);
-        this.readData = (Button) findViewById(R.id.read_button);
-        this.mName = (EditText) findViewById(R.id.name);
-        this.mAge = (EditText) findViewById(R.id.age);
-        this.mOutputText = (TextView) findViewById(R.id.text_view);
+        Execute_Button = (Button) findViewById(R.id.execute_button);
+        readData = (Button) findViewById(R.id.read_button);
+        mName = (EditText) findViewById(R.id.name);
+        mAge = (EditText) findViewById(R.id.age);
+        mOutputText = (TextView) findViewById(R.id.text_view);
         FirebaseDatabase instance = FirebaseDatabase.getInstance();
-        this.mDatabase = instance;
-        this.mRef = instance.getReference("users");
+        mDatabase = instance;
+        mRef = instance.getReference("users");
     }
 
     /* access modifiers changed from: protected */
     public void onDestroy() {
         super.onDestroy();
         this.mRef.child("user 1").removeEventListener(this.mListner);
+        this.mRef.child("user 1").removeEventListener(childListner);
     }
 }
---
+++ app/src/main/java/com/example/firebasedatabase/model/User.java
@@ -0,0 +1,32 @@
+package com.example.firebasedatabase.model;
+
+public class User {
+    String name;
+    String age;
+
+    public User(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public User() {
+    }
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
